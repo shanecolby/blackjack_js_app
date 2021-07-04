ec3d04d0cfac26606f68dafaf0614b6826c3e2cc
shanecolby <shanecolbyzero8@gmail.com>
2021-07-04 08:32:27 -0500

    add all functions to make basic blackjack app

--- index.js
+++ index.js
@@ -13,3 +13,51 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
 
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+  let randomCard = Math.floor(Math.random() * 13) + 1
+  if (randomCard > 10) {
+    return 10
+  } else if (randomCard === 1) {
+    return 11
+  } else {
+    return randomCard
+  }
+}
+
+function startGame() {
+  isAlive = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  cards = [firstCard, secondCard]
+  sum = firstCard + secondCard
+  renderGame()
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: "
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " "
+  }
+  sumEl.textContent = "Sum: " + sum
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+  } else if (sum === 21) {
+    message = "You've got Blackjack!"
+    hasBlackJack = true
+  } else {
+    message = "You're out of the game!"
+    isAlive = false
+  }
+  messageEl.textContent = message
+}
+
+function newCard() {
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+  }
+}
