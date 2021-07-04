3c4c05f16124608486263fbb589f62900c53baad
shanecolby <shanecolbyzero8@gmail.com>
2021-07-04 08:20:50 -0500

    add index.js and begin game structure

---
+++ index.js
@@ -0,0 +1,15 @@
+let player = {
+  name: "Player1",
+  chips: 500
+}
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
